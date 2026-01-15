49c5f9d359c99cf225afc9e6e89240a8f07c9768
afzalhussainj <afzalhussainjatt@gmail.com>
2026-01-15 19:40:50 +0500

    url orders chaNGED

--- CRM/leads/urls.py
+++ CRM/leads/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path("<str:pk>/", views.LeadDetailView.as_view()),
     path("<str:pk>/follow-up-status/", views.LeadFollowUpStatusUpdateView.as_view(), name="api_lead_follow_up_status"),
     path("<str:pk>/always-active/", views.LeadAlwaysActiveUpdateView.as_view(), name="api_lead_always_active"),
-    path("<str:pk>/notes/", views.LeadNotesListView.as_view(), name="api_lead_notes"),
-    path("<str:pk>/notes/unread/", views.LeadNotesUnreadListView.as_view(), name="api_lead_notes_unread"),
-    path("<str:pk>/notes/<str:note_pk>/", views.LeadNoteDetailView.as_view(), name="api_lead_note_detail"),
     path("<str:pk>/notes/mark-read/", views.LeadNoteMarkReadView.as_view(), name="api_lead_notes_mark_read"),
+    path("<str:pk>/notes/unread/", views.LeadNotesUnreadListView.as_view(), name="api_lead_notes_unread"),
+    path("<str:pk>/notes/", views.LeadNotesListView.as_view(), name="api_lead_notes"),
+    path("<str:pk>/notes/<str:note_pk>/", views.LeadNoteDetailView.as_view(), name="api_lead_note_detail"),
 ]
